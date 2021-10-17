2cf8a6011986ba82d11c828e8b1f68d2ba1a3d61
Artem-life <72021798+Artem-life@users.noreply.github.com>
2021-10-17 17:15:51 +0300

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,3 @@
+
+let list_nav=document.querySelector('.site-list-nav');let button_slose=document.querySelector('.button-slose');let button__list=document.querySelector('.main-button-list');button_slose.onclick=function(){list_nav.style.display='block';button__list.style.display='block';}
+button__list.onclick=function(){list_nav.style.display='none';button__list.style.display='none';}
